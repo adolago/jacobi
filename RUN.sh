d05ceb50524a868904cd54d652f2255222e62ece
Hans Christian Falkow <hc.falkow@gmail.com>
2026-01-07 12:28:26 +0100

    Updated files from TUWEL (v3)

--- RUN.sh
+++ RUN.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+
 [ -d results ] || mkdir results
 
 tol=1e-6
 max_iters=1000000
 
-for ((i=3;i<5;i++));do
+for ((i=3;i<4;i++));do
+#for ((i=8;i<10;i++));do
    nx=$((2**i+1))
    odir=`printf "results/NX%04d" ${nx}`
    [ -d ${odir} ] || mkdir ${odir}
